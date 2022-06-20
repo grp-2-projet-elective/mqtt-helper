12092a8233a1c098e50fe6439fef80bb2d91aa8b
felicienfouillet <felicienfouillet@live.fr>
2022-06-20 11:37:58 +0200

    (fix): remove mqtt connection config

--- src/models/esb.model.ts
+++ src/models/esb.model.ts
@@ -1,15 +1,3 @@
-import { IClientOptions } from "mqtt";
-
-export const mqttClientOptions: IClientOptions = {
-    port: 1883,
-    protocolVersion: 5,
-    keepalive: 60,
-    properties: {
-        requestResponseInformation: true,
-        requestProblemInformation: true,
-    },
-};
-
 export interface ResponseMessage {
     error: boolean;
     payload: string;
